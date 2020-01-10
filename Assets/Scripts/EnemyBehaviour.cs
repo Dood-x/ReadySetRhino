70de4af34dc48f8f090854a1a321ec30e9c12530
Dood-x <Dood-x@users.noreply.github.com>
2020-01-10 11:51:58 +0100

    enemy behaviour and perception added

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -6,17 +6,31 @@ public class EnemyBehaviour : MonoBehaviour
 {
 
     public float moveSpeed;
-    public Rigidbody2D rb;
-    public CircleCollider2D hitBox;
+    Rigidbody2D rb;
+    CircleCollider2D hitBox;
+    EnemyPerception ep;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ep = GetComponent<EnemyPerception>();
+        rb = GetComponent<Rigidbody2D>();
+        hitBox = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //movement if needed
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "HurtBox")
+        {
+            //die
+            hitBox.enabled = false;
+            Destroy(ep);
+            Destroy(this);
+        }
     }
 }
