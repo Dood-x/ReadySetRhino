e5d44965e83e6ac73beecee1e647baf0a32ecc83
Dood-x <Dood-x@users.noreply.github.com>
2020-01-10 11:20:34 +0100

    Changed Frans folder location

---
+++ Assets/Scripts/EnemyPerception.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPerception : MonoBehaviour
+{
+    public float sightDistance = 5.0f;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Phys
+    }
+}
