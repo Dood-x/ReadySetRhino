c53fe00371b58b030bdc860a2a9f55da9675713a
pinta1008 <57998225+pinta1008@users.noreply.github.com>
2020-01-10 11:19:04 +0100

    da

---
+++ Fran/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float moveSpeed = 7f;
+
+    Rigidbody2D rb;
+    GameObject player;
+    Vector2 moveDirection;
+
+     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        moveDirection = (player.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 3f);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name.Equals("Player"))
+        {
+            Debug.Log("Hit!");
+            Destroy(gameObject);
+        }
+    }
+}
